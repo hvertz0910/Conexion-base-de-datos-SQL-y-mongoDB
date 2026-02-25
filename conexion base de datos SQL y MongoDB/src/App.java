558b4fc72b1b3b6f408e8303d1fdd6bb15c0932f
hvertz0910 <hanniatrevinoe@gmail.com>
2026-02-24 21:09:31 -0600

    Se hizo el file de SQUL

--- conexion base de datos SQL y MongoDB/src/App.java
+++ conexion base de datos SQL y MongoDB/src/App.java
@@ -1,5 +1,12 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       // System.out.println("Hello, World!");
+
+
+        Custumer c1 = new Custumer (1, "John", "Doe", 30, "joe.doe@example.com");
+        Custumer c2 = new Custumer (2, "Pablo", "Martinez" , 20 , "plablo.martinez@example.com"); 
+
+        MySqlCRUD conn = ner MySqulCRUD();
     }
 }
